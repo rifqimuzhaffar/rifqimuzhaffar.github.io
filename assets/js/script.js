39d97d0103c15c831614553a1ec4bd45d86b777a
Rifqi Muzhaffar <rifqi.city@gmail.com>
2024-01-22 19:11:33 +0700

    menambahkan form submit to googlesheet

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,3 +21,19 @@ document.querySelector(".menu ul").addEventListener("click", (e) => {
     console.log("Checkbox unchecked!");
   }
 });
+
+// Kirim Form Feedback
+window.addEventListener("load", function () {
+  const form = document.getElementById("my-form");
+  form.addEventListener("submit", function (e) {
+    e.preventDefault();
+    const data = new FormData(form);
+    const action = e.target.action;
+    fetch(action, {
+      method: "POST",
+      body: data,
+    }).then(() => {
+      alert("Send Success!");
+    });
+  });
+});
